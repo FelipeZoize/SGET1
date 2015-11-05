2723af6f3449acbd5b145d1f94aad8d41f52b279
Donizette Domingues <donizettedomingues@gmail.com>
2015-11-05 20:35:09 -0300

    criado o model,dao,mbean,msgs e form de Agenda.

---
+++ SgetEJB/ejbModule/com/sgetejb/dao/AgendaDAO.java
@@ -0,0 +1,16 @@
+package com.sgetejb.dao;
+
+import javax.ejb.Local;
+import javax.ejb.Stateless;
+
+import com.sgetejb.model.Agenda;
+
+@Stateless
+public class AgendaDAO extends GenericDAO<Agenda> {
+
+	public AgendaDAO(){
+		super(Agenda.class);
+	}
+	
+
+}
